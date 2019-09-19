6133ee842b00bb20ffae4327e42abf7e4858a3b8
kman4you <khubaibahmad1994@gmail.com>
2019-09-19 16:43:32 +0500

    add API endpoint

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
+import config from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
+import personRoutes from './server/routes/PersonRoutes';
+
+config.config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 8000;
+
+app.use('/api/v1/persons', personRoutes);
+
 // when a random route is inputed
 app.get('*', (req, res) => res.status(200).send({
         message: 'Welcome to this API.'
